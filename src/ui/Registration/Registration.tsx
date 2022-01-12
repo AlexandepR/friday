0ae15aeee424fed38f13b752859be4cbd606540f
Yauheni Kapachenia <kapachenia@gmail.com>
2022-01-12 20:55:23 +0300

    add form registration

--- src/ui/Registration/Registration.tsx
+++ src/ui/Registration/Registration.tsx
@@ -1,5 +1,32 @@
-import React from "react";
+import React, {useState} from "react"
+import s from "./Registration.module.css"
 
 export const Registration = () => {
-    return <div>Registration test</div>;
-};
+
+    console.log('component Registration render')
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSubmit = () => {
+        // e.preventDefault();
+        console.log(email, password)
+    }
+
+    return (
+        <div className={s.wrapper}>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label className={s.label}>Email</label>
+                    <input type='email' value={email} onChange={(e) => setEmail(e.target.value)}/>
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input type='password' value={password} onChange={(e) => setPassword(e.target.value)}/>
+                </div>
+                <button>Registration{handleSubmit}</button>
+            </form>
+        </div>
+    )
+}
+
