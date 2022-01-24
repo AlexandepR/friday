27026308d8f33fd72de616b9713f2bf9ba633692
Alex <alexandepp@gmail.com>
2022-01-24 13:43:23 +0300

    add login

--- src/ui/LogIn/LogInAPI.tsx
+++ src/ui/LogIn/LogInAPI.tsx
@@ -3,8 +3,8 @@ import {LoginResponseType, LoginType} from "./LogInTypes";
 
 
 const instance = axios.create({
-    // baseURL: 'https://neko-back.herokuapp.com/2.0',
-    baseURL: 'http://localhost:7542/2.0',
+    baseURL: 'https://neko-back.herokuapp.com/2.0',
+    // baseURL: 'http://localhost:7542/2.0',
     withCredentials: true
 })
 
